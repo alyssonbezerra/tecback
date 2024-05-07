32aee8e4b330e47f5762e180ca788c76d81966e6
alyssonbezerra <alyssonuniesp@gmail.com>
2024-05-07 20:22:56 -0300

    Inclusão do Cartao

---
+++ src/main/java/br/com/fujideia/iesp/tecback/model/Cartao.java
@@ -0,0 +1,13 @@
+package br.com.fujideia.iesp.tecback.model;
+
+import lombok.Data;
+
+@Data
+public class Cartao {
+
+    private  String numero;
+    private String codigoSeguranca;
+    private String nomeTitular;
+    private String dataValidade;
+
+}
